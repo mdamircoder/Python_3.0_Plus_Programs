cecdcce30c48ee9790ff951a7b9b28cbfe98f3ce
Md Amir Sohail <mdas442006@gmail.com>
2017-08-22 15:20:16 +0530

    Update Hello World.py

--- Hello World.py
+++ Hello World.py
@@ -2,3 +2,8 @@
 #It just prints the String "Hello World !"
 
 print ("Hello World !")
+
+# By default print function prints a new line at the end
+# If you don't want to print the new line at the end, then use the following
+# print ("Hello World !", end="")
+# By default end="\n" in the print function
