407a3530d3ad0c3977dc5d6e22f3ee26e6c14544
Md Amir Sohail <mdas442006@gmail.com>
2017-11-06 20:42:02 +0530

    GCD / HCF of two integers in Python

---
+++ GCD-or-HCF/gcd.py
@@ -0,0 +1,25 @@
+from math import gcd
+
+print( "1. GCD of 15 amd 6 is :", gcd(15, 6) )
+print( "2. GCD of 5 amd 6 is :", gcd(5, 6) )
+print( "3. GCD of 0 amd 6 is :", gcd(0, 6) )
+print( "4. GCD of -4 amd 10 is :", gcd(-4, 10) )
+print( "5. GCD of 4 amd -10 is :", gcd(4, -10) )
+print( "6. GCD of -4 amd -10 is :", gcd(-4, -10) )
+
+'''#######################################################
+
+gcd(argument_1, argument_2) is an inbuilt method in Python.
+** This method takes only two arguments (Integers).
+**  You just need to import it from library 'math'.
+
+Remember, gcd(-a, -b) = gcd(-a, b) = gcd(a, -b) = gcd(a, b)
+
+################ S A M P L E   O U T P U T ###############
+1. GCD of 15 amd 6 is : 3
+2. GCD of 5 amd 6 is : 1
+3. GCD of 0 amd 6 is : 6
+4. GCD of -4 amd 10 is : 2
+5. GCD of 4 amd -10 is : 2
+6. GCD of -4 amd -10 is : 2
+#######################################################'''
